829b3afcec48855911d9a07c0c427ffe2fd04578
dendraa <dendrasisi6@gmail.com>
2024-03-02 02:38:33 +0700

    galeri

---
+++ page/home.php
@@ -0,0 +1,26 @@
+<div class="container my-4 p-5 bg-hero rounded">
+    <div class="py-5 text-white">
+        <p class="display-5 fw-bold">GALLERY'S </p>
+        <p class="fs-4 fw-bold col-md-8">We'll tell you more about "Blue" in Art.</p>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        
+        <?php
+            $tampil=mysqli_query($conn, "SELECT * FROM foto INNER JOIN user ON foto.userid=user.userid");
+            foreach($tampil as $tampils):
+        ?>
+    <div class="col-6 col-md-4 col-lg-3">
+    <div class="card mt-3">
+  <img src="uploads/<?= $tampils['lokasifile'] ?>" class="object-fit-cover" style="aspect-ratio: 10/9" >
+  <div class="card-body">
+    <h5 class="card-title"><?= $tampils['judulfoto'] ?></h5>
+    <p class="card-text text-muted">by: <?= $tampils['username'] ?></p>
+    <a href="?url=detail&&id=<?= $tampils['fotoid'] ?>" class="btn btn-primary">detail</a>
+  </div>
+</div>
+    </div>
+    <?php endforeach; ?>
+    </div>
+</div>
